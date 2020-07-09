cbcf63251d0d8c6b0dda5ef80908dfda51529b4b
Schoss <33396314+Schoss@users.noreply.github.com>
2020-07-09 20:20:27 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,25 @@
+import alpha_vantage
+from alpha_vantage import *
+import json
+import requests
+import csv
+
+API_URL = "https://www.alphavantage.co/query" 
+symbols = ['QCOM',"INTC","PDD","AAPL"]
+
+for symbol in symbols:
+        data = { "function": "TIME_SERIES_INTRADAY", 
+        "symbol": symbol,
+        "interval" : "60min",       
+        "datatype": "json", 
+        "apikey": "39BAHPL391ODW3GE" } 
+        response = requests.get(API_URL, data) 
+        data = response.json()
+        print(symbol)
+        a = (data['Time Series (60min)'])
+        keys = (a.keys())
+        for key in keys:
+                print(a[key]['2. high'] + " " + a[key]['5. volume'])
+
+SomeCommand > SomeFile.txt  
+
